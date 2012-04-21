5c13e376bcc3f95a352607cb67123d81b111014d
acuddlyheadcrab <acuddlyheadcrab@gmail.com>
2012-04-21 13:47:22 -0700

    Changed some config keys, and got block protection working.
    Let me know if they're too confusing, please.

--- src/com/acuddlyheadcrab/MCHungerGames/HungerListener.java
+++ src/com/acuddlyheadcrab/MCHungerGames/HungerListener.java
@@ -1,6 +1,7 @@
 package com.acuddlyheadcrab.MCHungerGames;
 
 import com.acuddlyheadcrab.util.ConfigKeys;
+import com.acuddlyheadcrab.util.PluginInfo;
 import com.acuddlyheadcrab.util.Utility;
 import java.util.Iterator;
 import java.util.Set;
@@ -14,7 +15,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockCanBuildEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.inventory.CraftItemEvent;
@@ -37,58 +38,119 @@ public class HungerListener implements Listener {
     public void onBlockBreak(BlockBreakEvent e){
         Location loc = e.getBlock().getLocation();
 		
-        if(config.getBoolean(ConfigKeys.OPTS_BLOCKPROT_BREAK.key())){
+        if(!config.getBoolean(ConfigKeys.OPTS_BLOCKPROT_BREAK.key())){
             String arenakey = Arenas.getNearbyArena(loc);
             if(arenakey!=null){
                 if(config.getBoolean(ConfigKeys.OPTS_BLOCKPROT_INARENA.key())){
-                    if(config.getBoolean(ConfigKeys.OPTS_BLOCKPROT_INGAME.key())){
-                        if(!Arenas.isInGame(arenakey)) return;
+                    boolean onlyingame = config.getBoolean(ConfigKeys.OPTS_BLOCKPROT_INGAME.key());
+                    if(onlyingame){
+                        if(!Arenas.isInGame(arenakey)) 
+//                            temp debug
+                            if(config.getBoolean(ConfigKeys.OPTS_DEBUG_ONBLOCKCHANGE.key())) 
+                                PluginInfo.sendPluginInfo("Returning, because arena "+arenakey+" is not in game");
+                            return;
                     }
-                    e.setCancelled(config.getBoolean(ConfigKeys.OPTS_BLOCKPROT_BREAK.key()));
+                    
+                    if(config.getBoolean(ConfigKeys.OPTS_DEBUG_ONBLOCKCHANGE.key())) 
+                        PluginInfo.sendPluginInfo("Cancelled BREAK "+e.getBlock().getType().toString()+" inside "+arenakey+" ("+e.getPlayer().getName()+")");
+                    String 
+                        suffix = onlyingame ? "while "+arenakey+" is in game!" : "inside "+arenakey+"!",
+                        msg = ChatColor.RED+"You cannot break blocks "+suffix
+                    ;
+                    if(config.getBoolean(ConfigKeys.OPTS_BLOCKPROT_SHOWWARN.key()))
+                        e.getPlayer().sendMessage(msg);
+                    e.setCancelled(!config.getBoolean(ConfigKeys.OPTS_BLOCKPROT_BREAK.key()));
+                    return;
                 }
             } else {
                 if(config.getBoolean(ConfigKeys.OPTS_BLOCKPROT_OUTARENA.key())){
-                    e.setCancelled(config.getBoolean(ConfigKeys.OPTS_BLOCKPROT_BREAK.key()));
+                    if(config.getBoolean(ConfigKeys.OPTS_DEBUG_ONBLOCKCHANGE.key())) 
+                        PluginInfo.sendPluginInfo("Cancelled BREAK "+e.getBlock().getType().toString()+" outside arenas ("+e.getPlayer().getName()+")");
+                    String msg = ChatColor.RED+"You cannot break blocks outside of arenas!";
+                    if(config.getBoolean(ConfigKeys.OPTS_BLOCKPROT_SHOWWARN.key()))
+                        e.getPlayer().sendMessage(msg);
+                    e.setCancelled(!config.getBoolean(ConfigKeys.OPTS_BLOCKPROT_BREAK.key()));
+                    return;
                 }
             }
         }
     }
 	
-	public void onBlockCanBuild(BlockCanBuildEvent e){
+    @EventHandler(priority = EventPriority.HIGHEST)
+	public void onBlockCanBuild(BlockPlaceEvent e){
 		Location loc = e.getBlock().getLocation();
 		
-		if(config.getBoolean(ConfigKeys.OPTS_BLOCKPROT_BUILD.key())){
+		if(!config.getBoolean(ConfigKeys.OPTS_BLOCKPROT_BUILD.key())){
             String arenakey = Arenas.getNearbyArena(loc);
             if(arenakey!=null){
                 if(config.getBoolean(ConfigKeys.OPTS_BLOCKPROT_INARENA.key())){
-                    if(config.getBoolean(ConfigKeys.OPTS_BLOCKPROT_INGAME.key())){
-                        if(!Arenas.isInGame(arenakey)) return;
+                    boolean onlyingame = config.getBoolean(ConfigKeys.OPTS_BLOCKPROT_INGAME.key());
+                    if(onlyingame){
+                        if(!Arenas.isInGame(arenakey)) 
+                            if(config.getBoolean(ConfigKeys.OPTS_DEBUG_ONBLOCKCHANGE.key())) 
+                                PluginInfo.sendPluginInfo("Returning, because arena "+arenakey+" is not in game");
+                            return;
                     }
-                    e.setBuildable(config.getBoolean(ConfigKeys.OPTS_BLOCKPROT_BUILD.key()));
+                    
+                    if(config.getBoolean(ConfigKeys.OPTS_DEBUG_ONBLOCKCHANGE.key())) 
+                        PluginInfo.sendPluginInfo("Cancelled BUILD "+e.getBlock().getType().toString()+" inside "+arenakey+" ("+e.getPlayer().getName()+")");
+                    String 
+                        suffix = onlyingame ? "while "+arenakey+" is in game!" : "inside "+arenakey+"!",
+                        msg = ChatColor.RED+"You cannot place blocks "+suffix
+                    ;
+                    if(config.getBoolean(ConfigKeys.OPTS_BLOCKPROT_SHOWWARN.key()))
+                        e.getPlayer().sendMessage(msg);
+                    e.setCancelled(!config.getBoolean(ConfigKeys.OPTS_BLOCKPROT_BUILD.key()));
+                    return;
                 }
             } else {
                 if(config.getBoolean(ConfigKeys.OPTS_BLOCKPROT_OUTARENA.key())){
-                    e.setBuildable(config.getBoolean(ConfigKeys.OPTS_BLOCKPROT_BUILD.key()));
+                    if(config.getBoolean(ConfigKeys.OPTS_DEBUG_ONBLOCKCHANGE.key())) 
+                        PluginInfo.sendPluginInfo("Cancelled BUILD "+e.getBlock().getType().toString()+" outside arenas ("+e.getPlayer().getName()+")");
+                    String msg = ChatColor.RED+"You cannot place blocks outside of arenas!";
+                    if(config.getBoolean(ConfigKeys.OPTS_BLOCKPROT_SHOWWARN.key()))    
+                        e.getPlayer().sendMessage(msg);
+                    e.setCancelled(!config.getBoolean(ConfigKeys.OPTS_BLOCKPROT_BUILD.key()));
+                    return;
                 }
             }
         }
 	}
 	
+    @EventHandler(priority = EventPriority.HIGHEST)
 	public void onPlayerCraft(CraftItemEvent e){
 		Location loc = e.getWhoClicked().getLocation();
 		
-		if(config.getBoolean(ConfigKeys.OPTS_BLOCKPROT_CRAFT.key())){
+		if(!config.getBoolean(ConfigKeys.OPTS_BLOCKPROT_CRAFT.key())){
             String arenakey = Arenas.getNearbyArena(loc);
             if(arenakey!=null){
                 if(config.getBoolean(ConfigKeys.OPTS_BLOCKPROT_INARENA.key())){
-                    if(config.getBoolean(ConfigKeys.OPTS_BLOCKPROT_INGAME.key())){
-                        if(!Arenas.isInGame(arenakey)) return;
+                    boolean onlyingame = config.getBoolean(ConfigKeys.OPTS_BLOCKPROT_INGAME.key());
+                    if(onlyingame){
+                        if(!Arenas.isInGame(arenakey)) 
+                            if(config.getBoolean(ConfigKeys.OPTS_DEBUG_ONBLOCKCHANGE.key())) 
+                                PluginInfo.sendPluginInfo("Returning, because arena "+arenakey+" is not in game");
+                            return;
                     }
-                    e.setCancelled(config.getBoolean(ConfigKeys.OPTS_BLOCKPROT_CRAFT.key()));
+                    
+                    if(config.getBoolean(ConfigKeys.OPTS_DEBUG_ONBLOCKCHANGE.key())) 
+                        PluginInfo.sendPluginInfo("Cancelled CRAFT inside "+arenakey+" ("+e.getWhoClicked().getName()+")");
+                    String 
+                        suffix = onlyingame ? "while "+arenakey+" is in game!" : "inside "+arenakey+"!",
+                        msg = ChatColor.RED+"You cannot craft "+suffix
+                    ;
+                    if(e.getWhoClicked() instanceof Player) ((Player) e.getWhoClicked()).sendMessage(msg);
+                    e.setCancelled(!config.getBoolean(ConfigKeys.OPTS_BLOCKPROT_CRAFT.key()));
+                    return;
                 }
             } else {
                 if(config.getBoolean(ConfigKeys.OPTS_BLOCKPROT_OUTARENA.key())){
-                    e.setCancelled(config.getBoolean(ConfigKeys.OPTS_BLOCKPROT_CRAFT.key()));
+                    if(config.getBoolean(ConfigKeys.OPTS_DEBUG_ONBLOCKCHANGE.key())) 
+                        PluginInfo.sendPluginInfo("Cancelled CRAFT outside arenas ("+e.getWhoClicked().getName()+")");
+                    String msg = ChatColor.RED+"You cannot craft outside of an arena!";
+                    if(e.getWhoClicked() instanceof Player) ((Player) e.getWhoClicked()).sendMessage(msg);
+                    e.setCancelled(!config.getBoolean(ConfigKeys.OPTS_BLOCKPROT_CRAFT.key()));
+                    return;
                 }
             }
         }
